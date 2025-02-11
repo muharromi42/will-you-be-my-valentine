06f34738b174be0860783718c1b2dd97b2a4847c
muharromi42 <muharromi42@gmail.com>
2025-02-11 21:49:08 +0700

    change gif and phrases

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,15 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
+      "Beneran?",
       "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Yakinnnnn???",
+      "Coklatt lohhh",
+      "Nanti nyesellllll",
+      "Udah di depan btw",
+      "Aokaowoawawk",
+      "Gk bisa nolak ya :v",
+      "Dah ambil aja depan rumah",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -37,16 +31,16 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img src="https://media1.tenor.com/m/_yUy24JPU7MAAAAd/suga-finger-hearts-yoongi-finger-hearts.gif" />
+          <div className="my-4 text-4xl font-bold">Nihh depan rumah hehe</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media1.tenor.com/m/NzRsT9jd374AAAAC/bts-suga.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Mau coklat?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
